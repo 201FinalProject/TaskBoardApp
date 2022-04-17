0d5f03ade0b55745a0c7cf01e1d62ac92b2c1a9a
aadeshbajaj <aadeshbajaj@localhost>
2022-04-17 15:31:04 -0700

    Aadesh First Commit 4/17 3:26 pm

--- CSCI201FinalProject/src/main/java/LoginDispatcher.java
+++ CSCI201FinalProject/src/main/java/LoginDispatcher.java
@@ -1,6 +1,13 @@
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginDispatcher")
 public class LoginDispatcher extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String url = "jdbc:mysql://localhost:3306/ChoreboardDatabase";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,7 +35,63 @@ public class LoginDispatcher extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("errorMessage", "");
+
+		
+		String loginEmail = request.getParameter("loginEmail");
+		String loginPW = request.getParameter("loginPassword");
+		
+		//Ensure both fields are not empty otherwise redirect back to auth.jsp
+		if (loginPW == null || loginPW == "" || loginEmail == null || loginEmail == "") {
+			RequestDispatcher requestDispatcher = request
+		            .getRequestDispatcher("auth.jsp"); //CHANGE to home page name
+		    requestDispatcher.forward(request, response);
+		} 
+
+		//Check to see if loginEmail contains proper ending, otherwise redirect back to auth.jsp with error message
+		if (!loginEmail.contains(".com") && !loginEmail.contains(".net") && !loginEmail.contains(".gov")
+				&& !loginEmail.contains(".edu") && !loginEmail.contains(".org") && !loginEmail.contains(".co.uk")
+				&& !loginEmail.contains(".biz") && !loginEmail.contains(".info")) {
+			RequestDispatcher requestDispatcher = request
+		            .getRequestDispatcher("auth.jsp"); //CHANGE to home page name
+		    requestDispatcher.forward(request, response);
+		}
+		
+		
+		String sql = "SELECT name FROM user WHERE email=? AND password=?"; //CHANGE based on database name
+		String user = "root";//CHANGE
+		String pwd = "root";//CHANGE
+		
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception ex) {
+            // handle the error
+        	System.out.println("SQLException: " + ex.getMessage());
+        }
+		
+		ResultSet rs = null;
+		
+		try {
+			Connection c = DriverManager.getConnection(url, user, pwd);
+			PreparedStatement p = c.prepareStatement(sql);
+			p.setString(1, loginEmail);
+			p.setString(2, loginPW);
+		    rs = p.executeQuery();
+		    if (rs.next() == false) {
+		    	throw new Exception("User Does Not Exist");
+		    }
+		} 
+		catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+		catch (Exception e) {
+			//handle somehow by printing right error message on screen with jsp
+			request.setAttribute("errorMessage", "Invalid email or password. Or, bad Google login. Please try again.");
+			RequestDispatcher requestDispatcher = request
+		            .getRequestDispatcher("auth.jsp");
+		    requestDispatcher.forward(request, response);
+		}
+		
 	}
 
 	/**
