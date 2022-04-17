a30d4eccf4783540974c2d1d3e5b5a327666a277
Matthew Quan <maquan@usc.edu>
2022-04-17 15:45:22 -0700

    history and util classes

---
+++ CSCI201FinalProject/src/main/java/Util/TaskArray.java
@@ -0,0 +1,17 @@
+package Util;
+import java.util.*;
+
+public class TaskArray {
+	private List<Task> tasks = null;
+
+	public List<Task> getTasks() {
+	return tasks;
+	}
+	
+	public void setTasks(List<Task> businesses) {
+	this.tasks = businesses;
+	}
+
+	
+
+}
