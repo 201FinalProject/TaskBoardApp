698c6c44b707eba8f1f3d2f2e8dfaa4135913428
Paul Kim <pbkim@usc.edu>
2022-04-24 16:52:07 -0700

    Update Register.jsx

---
+++ 201-react/src/Pages/Register.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import GroupSelect from "../Components/GroupSelectButton/GroupSelect"
+import PageHeader from "../Components/PageHeader/PageHeader"
+
+function Register(){
+    return(
+        <div style={{marginTop: "100px"}}>
+            <PageHeader></PageHeader>
+            <GroupSelect></GroupSelect>
+            <GroupSelect></GroupSelect>
+            <GroupSelect></GroupSelect>
+        </div>
+    )
+}
+
+export default HomePage;
