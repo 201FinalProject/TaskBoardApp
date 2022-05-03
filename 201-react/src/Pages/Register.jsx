89faa6bfbb40aa7dbe2f34fed63d54dd33e239b2
Aadesh Bajaj <aadeshbajaj@Aadeshs-MacBook-Pro.local>
2022-05-02 18:10:13 -0700

    fuck

--- 201-react/src/Pages/Register.jsx
+++ 201-react/src/Pages/Register.jsx
@@ -12,15 +12,17 @@ function Register(){
     return(
         <div id="Main" style={{marginTop: "100px"}}>
             <h1>Register</h1>
-            <div id="FormContainer">
                 <form>
                     <input id="Username" name="Username" placeholder="Username"/>
                     <input type="email" id="Email" name="Email" placeholder="Email"/>
                     <input type="password" id="Password" name="Password" placeholder="Password"/>
                     <input type="password" id="ConfirmPass" name="ConfirmPass" placeholder="Confirm Password"/>
-                    <button type="submit" name="Register">Create Account</button>
+                    <div class="container">
+                        <div class="vertical-center">
+                            <button type="submit" name="Register">Create Account</button>
+                            </div>
+                    </div>
                 </form>
-            </div>
         </div>
     )
 }
