443cf776854c59e1e0b45f6890f503fbf94176d5
Aditya Hariharan <adityaha@usc.edu>
2022-05-02 18:33:39 -0700

    last push adit

--- react-201/src/App.jsx
+++ react-201/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Navbar from './Components/navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 import Login from './Pages/login';
 import './App.css';
 import HomePage from './Pages/HomePage';
