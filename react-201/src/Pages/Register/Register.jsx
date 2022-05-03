abc4cbbc099c6aecd618379fde3b0c303ee7f78e
Aadesh Bajaj <aadeshbajaj@Aadeshs-MacBook-Pro.local>
2022-05-03 16:37:43 -0700

    updated login and register

---
+++ react-201/src/Pages/Register/Register.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GroupSelect from "../Components/GroupSelectButton/GroupSelect"
+import PageHeader from "../Components/PageHeader/PageHeader"
+import './Register.css';
+
+
+const useStyles ={
+
+
+}
+function Register(){
+    return(
+        <div id="Main" style={{marginTop: "100px"}}>
+            <h1>Register</h1>
+                <form>
+                    <div class="inputsContainer">
+                        <input id="Username" name="Username" placeholder="Username"/>
+                        <input type="email" id="Email" name="Email" placeholder="Email"/>
+                        <input type="password" id="Password" name="Password" placeholder="Password"/>
+                        <input type="password" id="ConfirmPass" name="ConfirmPass" placeholder="Confirm Password"/>
+                    </div>
+                    <div class="container">
+                        <div class="vertical-center">
+                            <button type="submit" name="Register">Create Account</button>
+                            </div>
+                    </div>
+                </form>
+        </div>
+    )
+}
+
+export default Register;
