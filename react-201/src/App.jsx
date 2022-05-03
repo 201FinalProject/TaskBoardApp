f3eb49271db59bc61f6e8b93675c425fec002391
Aditya Hariharan <adityaha@usc.edu>
2022-05-02 18:49:09 -0700

    hi

--- react-201/src/App.jsx
+++ react-201/src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './Pages/Home/HomePage';
 import Register from './Pages/Register/Register';
 import HistoryPage from './Pages/History/HistoryPage';
 import CreateGroup from './Pages/CreateGroup/CreateGroup';
+import TaskCreate from './Pages/TaskCreate/TaskCreate';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/history" element={<HistoryPage/>}/>
             <Route path="/Register" element={<Register></Register>}/>
             <Route path="/CreateGroup" element={<CreateGroup/>}/>
+            <Route path="/taskcreate" element={<TaskCreate/>}/>
         </Routes>
       </div>
     </BrowserRouter>
