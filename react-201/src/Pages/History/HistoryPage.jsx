bcbb29ec77e9a5f7c59bd1d6db06ccea936987a9
smgable <89372729+smgable@users.noreply.github.com>
2022-05-03 18:10:44 -0700

    SlaterBackground

--- react-201/src/Pages/History/HistoryPage.jsx
+++ react-201/src/Pages/History/HistoryPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PageHeader from "../../Components/PageHeader/PageHeader";
 import HistoryTask from "../../Components/HistoryTask/HistoryTask";
+import background from "../../LongBackground.png"
 
 function HistoryPage(){
     return(
-        <div style={{marginTop: "100px"}}>
-            <p id="PHeader">History</p>
+        <div style={{width: "100%",
+        backgroundImage: `url(${background}`,
+        backgroundSize: "cover",
+        height: "2048px"}}>
+            <PageHeader name="History"></PageHeader>
             <HistoryTask></HistoryTask>
             <HistoryTask></HistoryTask>
             <HistoryTask></HistoryTask>
