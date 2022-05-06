a20be3f5715fdfdf7f8e176e58c2b56620631a49
Matthew Quan <maquan@usc.edu>
2022-05-06 15:24:57 -0700

    fs

--- Full-Stack/react-frontend/src/services/HomePageService.js
+++ Full-Stack/react-frontend/src/services/HomePageService.js
@@ -23,6 +23,27 @@ class HomePageService {
     deleteGroup(groupId){
         return axios.delete(GROUP_API_BASE_URL + '/' + groupId);
     }
+    
+    getTasks(){
+        return axios.get(GROUP_API_BASE_URL);
+    }
+
+    createTask(task){
+        return axios.post(GROUP_API_BASE_URL, task);
+    }
+
+    getTaskById(taskId){
+        return axios.get(GROUP_API_BASE_URL + '/' + taskId);
+    }
+
+    updateTask(task, taskId){
+        return axios.put(GROUP_API_BASE_URL + '/' + taskId, task);
+    }
+
+    deleteTask(taskId){
+        return axios.delete(GROUP_API_BASE_URL + '/' + taskId);
+    }
+
 }
 
 export default new HomePageService()
