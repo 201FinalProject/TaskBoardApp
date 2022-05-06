69f691c7792883f1ace6a33b608c6dee05fdcb6e
Paul Kim <pbkim@usc.edu>
2022-05-05 22:11:58 -0700

    s

--- Full-Stack/react-frontend/src/Pages/ChatPage/Chat.jsx
+++ Full-Stack/react-frontend/src/Pages/ChatPage/Chat.jsx
@@ -75,7 +75,7 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt').limit(500);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
